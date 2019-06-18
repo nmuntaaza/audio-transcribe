0cf7bba5d5e1bd9fce38713d1e5d7eeed388423c
nmuntaaza <nmuntaaza.wh@gmail.com>
2019-06-19 00:23:33 +0700

    add google-speech-to-text depencency

--- requirements.py
+++ requirements.py
@@ -1,6 +1,9 @@
 appnope==0.1.0
 audioread==2.1.8
 backcall==0.1.0
+cachetools==3.1.1
+certifi==2019.6.16
+chardet==3.0.4
 Click==7.0
 cycler==0.10.0
 decorator==4.4.0
@@ -9,7 +12,13 @@ ffmpeg-python==0.1.18
 Flask==1.0.2
 Flask-WTF==0.14.2
 future==0.17.1
+google-api-core==1.11.1
+google-auth==1.6.3
+google-cloud-speech==1.0.0
+googleapis-common-protos==1.6.0
+grpcio==1.21.1
 hmmlearn==0.2.2
+idna==2.8
 ipykernel==5.1.1
 ipython==7.5.0
 ipython-genutils==0.2.0
@@ -30,7 +39,10 @@ parso==0.4.0
 pexpect==4.7.0
 pickleshare==0.7.5
 prompt-toolkit==2.0.9
+protobuf==3.8.0
 ptyprocess==0.6.0
+pyasn1==0.4.5
+pyasn1-modules==0.2.5
 -e git+git@github.com:nmuntaaza/audio_separation.git@60d7b83e6ee66b700722162b2b9ff3eb316f0a9f#egg=pyAudioAnalysis&subdirectory=libs/pyAudioAnalysis
 pydub==0.23.1
 Pygments==2.4.2
@@ -39,8 +51,11 @@ pysndfx==0.3.6
 python-dateutil==2.8.0
 python-dotenv==0.10.2
 python-magic==0.4.15
+pytz==2019.1
 pyzmq==18.0.1
+requests==2.22.0
 resampy==0.2.1
+rsa==4.0
 scikit-learn==0.21.2
 scipy==1.3.0
 simplejson==3.16.0
@@ -48,6 +63,7 @@ six==1.12.0
 sklearn==0.0
 tornado==6.0.2
 traitlets==4.3.2
+urllib3==1.25.3
 wcwidth==0.1.7
 Werkzeug==0.15.2
 WTForms==2.2.1
