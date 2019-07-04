cfc5e91ad46d08c91b7058cbc3a72657ab0da982
Naufal Muntaaza <nmuntaaza.wh@gmail.com>
2019-07-04 13:59:57 +0700

    add speech_to_text module

---
+++ web/transcribe/speech_to_text.py
@@ -0,0 +1,121 @@
+from operator import itemgetter
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+from google.cloud import storage
+
+
+def upload_to_bucket(blob_name, path_to_file, bucket_name):
+    """ Upload data to a bucket"""
+
+    # Explicitly use service account credentials by specifying the private key
+    # file.
+    storage_client = storage.Client()
+
+    #print(buckets = list(storage_client.list_buckets())
+
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.upload_from_filename(path_to_file)
+
+    #returns a public url
+    return blob.public_url
+
+
+def transcribe_audio(path):
+    """
+        Deskripsi: Function untuk melakukan transkripsi audio
+
+        Parameter:
+            path: String
+
+        Output:
+            response: array
+    """
+    # content = readbinaudio(path)
+    client = speech.SpeechClient()
+    audio = types.RecognitionAudio(uri=path)
+
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        language_code='id',
+        enable_word_time_offsets=True)
+
+    operation = client.long_running_recognize(config, audio)
+
+    response = operation.result(timeout=90)
+
+    for result in response.results:
+        print('Transcript: {}'.format(result.alternatives[0].transcript))
+
+    return response
+
+
+def process_transcript(response, label_speaker):
+    transcript = []
+    for result in response.results:
+        alternative = result.alternatives[0]
+        for word_info in alternative.words:
+            word = word_info.word
+            start_time = word_info.start_time
+            end_time = word_info.end_time
+
+            start_time = start_time.seconds + start_time.nanos * 1e-9
+            end_time = end_time.seconds + end_time.nanos * 1e-9
+
+            word_dict = {
+                "speaker": label_speaker,
+                "word": word,
+                "start_time": start_time,
+                "end_time": end_time
+            }
+
+            transcript.append(word_dict)
+
+    return transcript
+
+
+def sort_transcript(transcript1, transcript2):
+    transcript = transcript1
+    transcript.extend(transcript2)
+
+    sortedTranscript = sorted(transcript, key=itemgetter('start_time'), reverse=False)
+
+    return sortedTranscript
+
+
+def generate_dialogue(transcript):
+    sentence = ''
+    dialogue = []
+    first = True
+    pLabel = 0
+    label = transcript[0]['speaker']
+    start_time = transcript[0]['start_time']
+
+    for i in transcript:
+        if i['speaker'] == label:
+            sentence += i['word'] + ' '
+        else:
+            transcript = {
+                'timestamp': str(start_time),
+                'label': label,
+                'sentence': sentence
+            }
+            sentence = ''
+
+            dialogue.append(transcript)
+
+            sentence += i['word'] + ' '
+            first = False
+            label = i['speaker']
+            start_time = i['start_time']
+
+    transcript = {
+        'timestamp': str(start_time),
+        'label': label,
+        'sentence': sentence
+    }
+
+    dialogue.append(transcript)
+
+    return dialogue
--- web/transcribe/transcribe.py
+++ web/transcribe/transcribe.py
@@ -1,7 +1,8 @@
 import librosa
 from . import noise_reduction as nr
 from . import utils as apu
-from . import audio_diarization
+from . import audio_diarization as ad
+from . import speech_to_text as stt
 from flask import current_app
 
 
@@ -27,7 +28,27 @@ def transcribe(audio_file_name, noise_start_time=1, noise_end_time=3, verbose=Fa
             librosa.output.write_wav(audio_output_path, reduced_audio, audio_samplerate, norm=False)
         
         # #audio, time_trimmed = apu.trim_silence(audio)
+
+        # Diarization
+        speaker_speech = ad.audio_diarization(reduced_audio, audio_samplerate, 2, True)
+
+        # Masing masing speech disimpan dulu pada storage untuk digunakan saat conver_to_binary
+        temp_one = apu.write_to_wav(speaker_speech[0], audio_samplerate)
+        temp_two = apu.write_to_wav(speaker_speech[1], audio_samplerate)
+
+        path_one = "../wavtemp/{}.wav".format(temp_one)
+        path_two = "../wavtemp/{}.wav".format(temp_two)
+        blob_one = "tempfile/{}.wav".format(temp_one)
+        blob_two = "tempfile/{}.wav".format(temp_two)
+
+        bucket_one = stt.upload_to_bucket(blob_one, path_one, "kota-108")
+        bucket_two = stt.upload_to_bucket(blob_two, path_two, "kota-108")
+
+        apu.remove_file(path_one)
+        apu.remove_file(path_two)
+
     except ValueError as err:
+        print(err)
         return False
 
     return True
@@ -35,8 +56,6 @@ def transcribe(audio_file_name, noise_start_time=1, noise_end_time=3, verbose=Fa
 
 def reduce_noise(audio, audio_sr, noise_start_time, noise_end_time):
     audio_noise = nr.get_noise(audio, noise_start_time, noise_end_time, audio_sr)
-    print("get_noise")
     reduced_audio = nr.remove_noise(audio, audio_noise, verbose=True)
-    print("remove_noise")
 
     return reduced_audio
