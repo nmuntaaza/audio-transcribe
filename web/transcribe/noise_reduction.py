da309a0b6cf6bb05d2ab38969859e1ebeca71d98
Naufal Muntaaza <nmuntaaza.wh@gmail.com>
2019-07-06 21:53:24 +0700

    refactoring

--- web/transcribe/noise_reduction.py
+++ web/transcribe/noise_reduction.py
@@ -69,67 +69,90 @@ def remove_noise(audio_clip, noise_clip, n_grad_freq=2, n_grad_time=4, n_fft=409
         menampilkan plot dari algoritma
     """
     
-    if verbose: start = time.time()
+    if verbose:
+        start = time.time()
         
     # STFT pada noise
-    noise_stft = _stft(noise_clip, n_fft, hop_length, win_length) 
-    noise_stft_db = _amp_to_db(np.abs(noise_stft))# convert ke dB
+    noise_stft = _stft(noise_clip, n_fft, hop_length, win_length)
+    # convert ke dB
+    noise_stft_db = _amp_to_db(np.abs(noise_stft))
     
     # Menghitung statistik pada noise
-    mean_freq_noise = np.mean(noise_stft_db, axis =1)
-    std_freq_noise = np.std(noise_stft_db, axis =1)
+    mean_freq_noise = np.mean(noise_stft_db, axis=1)
+    std_freq_noise = np.std(noise_stft_db, axis=1)
     noise_thresh = mean_freq_noise+std_freq_noise*n_std_thresh
     
-    if verbose: print('STFT on noise:', td(seconds=time.time()-start)); start = time.time()
+    if verbose:
+        print('STFT on noise:', td(seconds=time.time()-start))
+        start = time.time()
         
     # STFT pada sinyal audio
-    if verbose: start = time.time()
+    if verbose:
+        start = time.time()
+
     sig_stft = _stft(audio_clip, n_fft, hop_length, win_length)
     sig_stft_db = _amp_to_db(np.abs(sig_stft))
     
-    if verbose: print('STFT on signal:',td(seconds=time.time()-start)); start = time.time()
+    if verbose:
+        print('STFT on signal:', td(seconds=time.time()-start))
+        start = time.time()
         
     # Menghitung nilai masing pada desibel
     mask_gain_dB = np.min(_amp_to_db(np.abs(sig_stft)))
     print(noise_thresh, mask_gain_dB)
     
     # Membuat filter smoothing untuk masking pada waktu dan frekuensi
-    smoothing_filter = np.outer(np.concatenate([np.linspace(0,1,n_grad_freq+1,endpoint=False),np.linspace(1,0,n_grad_freq+2)])[1:-1], 
-                       np.concatenate([np.linspace(0,1,n_grad_time+1,endpoint=False),np.linspace(1,0,n_grad_time+2)])[1:-1])
+    smoothing_filter = np.outer(np.concatenate([np.linspace(0, 1, n_grad_freq+1, endpoint=False),
+                                                np.linspace(1, 0, n_grad_freq+2)])[1:-1],
+                                np.concatenate([np.linspace(0, 1, n_grad_time+1, endpoint=False),
+                                                np.linspace(1, 0, n_grad_time+2)])[1:-1])
     smoothing_filter = smoothing_filter/np.sum(smoothing_filter)
     
     # Menghitung threshold pada setiap frekuensi/time bin
-    db_thresh = np.repeat(np.reshape(noise_thresh, [1,len(mean_freq_noise)]), np.shape(sig_stft_db)[1], axis = 0).T
+    db_thresh = np.repeat(np.reshape(noise_thresh, [1, len(mean_freq_noise)]), np.shape(sig_stft_db)[1], axis=0).T
     
     # Mask apabila sinyal diatas nilai threshold
     sig_mask = sig_stft_db<db_thresh
     
-    if verbose: print('Masking:', td(seconds=time.time()-start)); start = time.time()
+    if verbose:
+        print('Masking:', td(seconds=time.time()-start))
+        start = time.time()
         
     # Convolce mask dengan smoothing filter
     sig_mask = scipy.signal.fftconvolve(sig_mask, smoothing_filter,  mode='same')
     sig_mask = sig_mask*prop_decrease
     
-    if verbose: print('Mask convolution:', td(seconds=time.time()-start)); start = time.time()
+    if verbose:
+        print('Mask convolution:', td(seconds=time.time()-start))
+        start = time.time()
         
     # Mask sinyal audio
     sig_stft_db_masked = sig_stft_db *(1-sig_mask) + np.ones(np.shape(mask_gain_dB))*mask_gain_dB*sig_mask # mask real
     sig_imag_masked = np.imag(sig_stft)*(1-sig_mask)
     sig_stft_amp = ((_db_to_amp(sig_stft_db_masked)*np.sign(sig_stft))+(1j * sig_imag_masked))
     
-    if verbose: print('Mask application:', td(seconds=time.time()-start)); start = time.time()
+    if verbose:
+        print('Mask application:', td(seconds=time.time()-start))
+        start = time.time()
         
     # Mengembalikan sinyal audio kedalam bentuk time series
     recovered_signal = _istft(sig_stft_amp, hop_length, win_length)
     recovered_spec = _amp_to_db(np.abs(_stft(recovered_signal, n_fft, hop_length, win_length)))
     
-    if verbose: print('Signal recovery:', td(seconds=time.time()-start));
-    if visual: plot_spectrogram(noise_stft_db, title='Noise')
-    if visual: plot_statistics_and_filter(mean_freq_noise, std_freq_noise, noise_thresh, smoothing_filter)
-    if visual: plot_spectrogram(sig_stft_db, title='Signal')
-    if visual: plot_spectrogram(sig_mask, title='Mask applied')
-    if visual: plot_spectrogram(sig_stft_db_masked, title='Masked signal')
-    if visual: plot_spectrogram(recovered_spec, title='Recovered spectrogram')
+    if verbose:
+        print('Signal recovery:', td(seconds=time.time()-start))
+    if visual:
+        plot_spectrogram(noise_stft_db, title='Noise')
+    if visual:
+        plot_statistics_and_filter(mean_freq_noise, std_freq_noise, noise_thresh, smoothing_filter)
+    if visual:
+        plot_spectrogram(sig_stft_db, title='Signal')
+    if visual:
+        plot_spectrogram(sig_mask, title='Mask applied')
+    if visual:
+        plot_spectrogram(sig_stft_db_masked, title='Masked signal')
+    if visual:
+        plot_spectrogram(recovered_spec, title='Recovered spectrogram')
     
     return recovered_signal
 
